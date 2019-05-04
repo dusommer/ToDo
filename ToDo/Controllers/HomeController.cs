07b4d01bb727115354a33a967aadeb6d27898fcd
eduardo sommer <dusommer@hotmail.com>
2019-05-04 11:42:19 -0300

    asdasd

--- ToDo/Controllers/HomeController.cs
+++ ToDo/Controllers/HomeController.cs
@@ -26,16 +26,22 @@ namespace ToDo.Controllers
             {
                 List<Item> itens = new List<Item>();
                 List<Item> children = new List<Item>();
-                children.Add(new Item() { Name = "{1635945} [Prior 8] Erro ao Duplicar documento no contas a pagar - fora da data contábil", Id = 1, Position = 0 });
-                children.Add(new Item() { Name = "parent significado, definição parent: 1. a mother or father of a person or an animal: 2. a company that owns one or more other companies: 3. a person who gives", Id = 2, Position = 1 });
-                children.Add(new Item() { Name = "Nenhum trabalho sobre falsos cognatos pode estar completo sem a presença deste campeão de audiência. O substantivo “PARENT” não significa “parente”", Id = 3, Position = 2 });
-                itens.Add(new Item() { Name = "ToDo", Id = 1, Position = 0, Children = children });
-                itens.Add(new Item() { Name = "Doing", Id = 2, Position = 1, Children = children });
-                itens.Add(new Item() { Name = "Done", Id = 3, Position = 2 });
+                children.Add(new Item() { Description = "{1635945} [Prior 8] Erro ao Duplicar documento no contas a pagar - fora da data contábil", Id = 1, Position = 0 });
+                children.Add(new Item() { Description = "parent significado, definição parent: 1. a mother or father of a person or an animal: 2. a company that owns one or more other companies: 3. a person who gives", Id = 2, Position = 1 });
+                children.Add(new Item() { Description = "Nenhum trabalho sobre falsos cognatos pode estar completo sem a presença deste campeão de audiência. O substantivo “PARENT” não significa “parente”", Id = 3, Position = 2 });
+                itens.Add(new Item() { Description = "ToDo", Id = 1, Position = 0, Children = children });
+                itens.Add(new Item() { Description = "Doing", Id = 2, Position = 1, Children = children });
+                itens.Add(new Item() { Description = "Done", Id = 3, Position = 2 });
                 ListItem = itens;
             }
 
             return View(ListItem);
+        }
+
+        public ActionResult SaveNewItem(string description)
+        {
+            ListItem.Add(new Item() { Description = description, Id = ListItem.Count(), Position = ListItem.Count() });
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult UpdateItem(string itens)
--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -8,7 +8,7 @@ namespace ToDo.Models
     public class Item
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Description { get; set; }
         public int Position { get; set; }
         public List<Item> Children { get; set; }
     }
