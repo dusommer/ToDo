2a9c378323efc98a2c69ed688efd1d6b0ffa139e
eduardo sommer <dusommer@hotmail.com>
2019-05-05 22:18:10 -0300

    sadfasdf

--- ToDo/ServicesApi/IServiceApiItem.cs
+++ ToDo/ServicesApi/IServiceApiItem.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ToDo.Models.Arguments.Item;
 
 namespace ToDo.ServicesApi
 {
     public interface IServiceApiItem
     {
+        InsertItemResponse Insert(InsertItemRequest request);
+        UpdateItemResponse Update(UpdateItemRequest request);
+        ItemResponse GetById(int id);
+        List<ItemResponse> GetByLisItem(int id);
+        void Remove(int id);
     }
 }
--- ToDo/ServicesApi/ServiceApiItem.cs
+++ ToDo/ServicesApi/ServiceApiItem.cs
@@ -1,6 +1,39 @@
-﻿namespace ToDo.ServicesApi
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using ToDo.Models.Arguments.Item;
+using ToDo.Utils;
+
+namespace ToDo.ServicesApi
 {
     public class ServiceApiItem : IServiceApiItem
     {
+        public ItemResponse GetById(int id)
+        {
+            return JsonConvert.DeserializeObject<ItemResponse>(
+                ServiceApiUtil.ApiResponse("api/item/GetById?request=" + id, "GET"));
+        }
+
+        public List<ItemResponse> GetByLisItem(int id)
+        {
+            return JsonConvert.DeserializeObject<List<ItemResponse>>(
+                ServiceApiUtil.ApiResponse("api/item/GetByLisItem?request=" + id, "GET"));
+        }
+
+        public InsertItemResponse Insert(InsertItemRequest request)
+        {
+            return JsonConvert.DeserializeObject<InsertItemResponse>(
+                ServiceApiUtil.ApiResponse("api/item/Insert", "POST", request));
+        }
+
+        public void Remove(int id)
+        {
+            ServiceApiUtil.ApiResponse("api/item/Remove?request=" + id, "GET");
+        }
+
+        public UpdateItemResponse Update(UpdateItemRequest request)
+        {
+            return JsonConvert.DeserializeObject<UpdateItemResponse>(
+                ServiceApiUtil.ApiResponse("api/item/Update", "POST", request));
+        }
     }
 }
